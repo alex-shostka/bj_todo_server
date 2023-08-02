6b209d17361f616d5e5d9d318772aebb84dba91a
Шостка А.С <shostka@lanit.ru>
2023-08-02 15:30:40 +0700

    changed db setting

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.POSTGRES_DATABASE,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
   {
     dialect: "postgres",
     dialectModule: require("pg"),
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.POSTGRES_HOST,
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
   }
 );
