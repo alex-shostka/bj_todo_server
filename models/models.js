c06b71f006764d38c3e05b4c38247d3f9a97282c
Шостка А.С <shostka@lanit.ru>
2023-08-02 16:46:58 +0700

    fixed imports in models

--- models/models.js
+++ models/models.js
@@ -1,14 +1,14 @@
-const sequelize = require("../db");
+const pool = require("../db");
 const { DataTypes } = require("sequelize");
 
-const User = sequelize.define("user", {
+const User = pool.define("user", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, unique: true },
   password: { type: DataTypes.STRING },
   role: { type: DataTypes.STRING, defaultValue: "ADMIN" },
 });
 
-const Todo = sequelize.define("todo", {
+const Todo = pool.define("todo", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   username: { type: DataTypes.STRING, allowNull: false },
   todotext: { type: DataTypes.STRING, allowNull: false },
