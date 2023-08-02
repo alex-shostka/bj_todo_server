f65c69de8351dcdd4761509ec97fb7c3747444f9
Шостка А.С <shostka@lanit.ru>
2023-08-02 15:00:26 +0700

    require pg

--- db.js
+++ db.js
@@ -6,3 +6,16 @@ module.exports = new Sequelize(
   process.env.DB_PASSWORD,
   { dialect: "postgres", host: process.env.DB_HOST, port: process.env.DB_PORT }
 );
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Connect to PostgreSQL successfully!");
+});
+
+module.exports = pool;
