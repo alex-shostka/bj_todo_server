ffbed1264e1fb21b8b4b74b2b5a5d180a5715b54
Шостка А.С <shostka@lanit.ru>
2023-08-02 15:01:57 +0700

    add pg manually

--- db.js
+++ db.js
@@ -4,18 +4,11 @@ module.exports = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  { dialect: "postgres", host: process.env.DB_HOST, port: process.env.DB_PORT }
+  {
+    dialect: "postgres",
+    dialectModule: require("pg"),
+
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
 );
-
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-});
-
-pool.connect((err) => {
-  if (err) throw err;
-  console.log("Connect to PostgreSQL successfully!");
-});
-
-module.exports = pool;
